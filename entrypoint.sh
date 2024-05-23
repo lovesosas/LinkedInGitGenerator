48c949c91e3bec3caafd69b0f080236493dc40bd
fucklovesosa <100899623+lovesosas@users.noreply.github.com>
2024-05-22 18:26:37 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "======="
-git config --global user.name "#{GITHUB_ACTOR}"
-git config --global user.email "#{INPUT_EMAIL}"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
